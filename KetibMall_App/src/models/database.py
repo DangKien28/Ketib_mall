9510255f81a84583330c400c494d847b21f761e2
DangKien28 <trungkien228102005@gmail.com>
2026-03-09 00:20:03 +0700

    Update gitignore

--- KetibMall_App/src/models/database.py
+++ KetibMall_App/src/models/database.py
@@ -1,16 +1,17 @@
-# File: KetibMall_App/src/models/database.py
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-# Format: postgresql://[user]:[password]@localhost:[port]/[database_name]
-SQLALCHEMY_DATABASE_URL = "postgresql://root:Dtk.281005@localhost:5432/ketib_app_db"
+# Tự động tìm và load các biến từ file .env
+load_dotenv()
 
-# Tạo động cơ kết nối
+# Lấy chuỗi kết nối một cách an toàn
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError("Lỗi: Không tìm thấy DATABASE_URL trong file .env")
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-# Tạo phiên làm việc với Database
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base class để các file model khác kế thừa
 Base = declarative_base()
